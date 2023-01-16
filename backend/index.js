1d09ab1187af6f3ebf280d7b4db3d873875cff03
Ibad <ibad.src@gmail.com>
2023-01-16 23:44:46 +0500

    user controller/register api

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,37 @@
 const connectToMongo = require('./db');
 
-connectToMongo();
+
 
 
 const express = require('express')
+const colors = require('colors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+
+//dotenv config
+dotenv.config();
+//mongoDB connection
+connectToMongo();
+
+//rest Object
 const app = express()
-const port = 5000
 
 
+//middleware
 app.use(express.json())
+app.use(morgan('dev'))
 
+
+
+//routes
 app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+// Port
+const port = process.env.PORT || 5000
 
 
+app.listen(port, () =>{
+  console.log(`Server Running in ${process.env.NODE_MODE} mode on port ${process.env.PORT}`
+  .bgCyan.white
+  );
+});
 
-
-
-
-
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
- 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import Heading from './Heading'
 import {useNavigate} from 'react-router-dom'
+import { message } from 'antd';
+import axios from 'axios';
 
 const Register = () => {
-    let navigate = useNavigate();
+    const navigate = useNavigate();
+    const submitHandler = async (values) => {
+      try {
+        const res = await axios.post('/api/auth/register', values)
+        if(res.data.success){
+        message:success('Registered Successfully')
+        navigate('/login')
+        }else{
+          message.error(res.data.message)
+        }
+      } catch (error) {
+        console.log(error)
+        message.error('something went wrong')
+        
+      }
+    };
   return (
+    <>
     <section name='contact' className="py-20">
         <div className="max-w-[1200px] mx-auto px-4">
           <Heading title='Register Account' />
           <div className="bg-light-bg p-6 max-w-[30rem] mx-auto rounded">
-            <form action="#">
+            <form action="#" onSubmit={submitHandler}>
               <div className="flex flex-col mb-4">
                 <label className="capitalize text-black mb-1">your name :</label>
                 <input
@@ -64,6 +82,7 @@ const Register = () => {
           </div>
         </div>
       </section>
+      </>
   )
 }
 
